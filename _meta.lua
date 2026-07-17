6b7f80703b6b4a14580c57da8e0e976571e2e96c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-17 08:57:14 +0200

    chore: bump version to 1.1.4

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "hanoi",
     fullname    = _("Towers of Hanoi"),
     description = _("Move disks between pegs"),
-    version     = "1.1.3",
+    version     = "1.1.4",
 }
