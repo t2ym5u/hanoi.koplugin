200de4e4f657de29ab5436f220fca7d2c93250f7
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-28 13:31:11 +0200

    chore: bump version to 1.1.8

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "hanoi",
     fullname    = _("Towers of Hanoi"),
     description = _("Move disks between pegs"),
-    version     = "1.1.7",
+    version     = "1.1.8",
 }
